7322cbae64bf8e6be8ea9e8455491fd1c2125f1c
ryan-ern <ryanernanda100902@gmail.com>
2025-01-10 17:16:42 +0700

    ref (ui) : change layout & typography

--- resources/views/content/pages/pages-misc-error.blade.php
+++ resources/views/content/pages/pages-misc-error.blade.php
@@ -12,8 +12,14 @@
     <!-- Error -->
     <div class="misc-wrapper">
         <h1 class="mb-2 mx-2" style="font-size: 6rem;line-height: 6rem;">404</h1>
-        <h4 class="mb-2">Page Not Found 🙄</h4>
-        <p class="mb-10 mx-2">we couldn't find the page you are looking for</p>
+        <h4 class="mb-2">Halaman Tidak Ditemukan 🙄</h4>
+        <p class="mb-10 mx-2">Kamu sepertinya berada di jalan yang salah</p>
+        <div>
+            <a href="{{ Auth::check() ? (Auth::user()->role == 'admin' ? url('/dashboard') : url('/beranda')) : url('/') }}"
+                class="btn btn-primary text-center mb-7">
+                <i class="ri-home-4-line me-2"></i>Yuk kembali ke jalan yang benar
+            </a>
+        </div>
         <div class="d-flex justify-content-center mt-5">
             <img src="{{ asset('assets/img/illustrations/tree-3.png') }}" alt="misc-tree"
                 class="img-fluid misc-object d-none d-lg-inline-block">
@@ -22,10 +28,6 @@
             <div class="d-flex flex-column align-items-center">
                 <img src="{{ asset('assets/img/illustrations/404.png') }}" alt="misc-error" class="misc-model img-fluid z-1"
                     width="780">
-                <div>
-                    <a href="{{ Auth::check() ? (Auth::user()->role == 'admin' ? url('/dashboard') : url('/beranda')) : url('/') }}"
-                        class="btn btn-primary text-center my-6">Back to home</a>
-                </div>
             </div>
         </div>
     </div>
