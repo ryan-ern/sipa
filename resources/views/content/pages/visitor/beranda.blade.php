d9ba024771873002f965f5ba5c6de6dd1fc0e4d9
ryan-ern <ryanernanda100902@gmail.com>
2025-02-27 00:51:41 +0700

    rev (carousel) responsive on mobile

--- resources/views/content/pages/visitor/beranda.blade.php
+++ resources/views/content/pages/visitor/beranda.blade.php
@@ -7,154 +7,200 @@
 
 @section('title', 'Beranda')
 
-@section('content')
-    <div class="row gy-6">
-        <div class="col-md-12 col-lg-12 mb-4">
-            <div class="card">
-                <div class="card-body p-7">
-                    <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel"
-                        data-bs-interval="5000">
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
-                                class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                                aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                                aria-label="Slide 3"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
-                                aria-label="Slide 4"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
-                                aria-label="Slide 5"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5"
-                                aria-label="Slide 6"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6"
-                                aria-label="Slide 7"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7"
-                                aria-label="Slide 8"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8"
-                                aria-label="Slide 9"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9"
-                                aria-label="Slide 10"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="10"
-                                aria-label="Slide 11"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="11"
-                                aria-label="Slide 12"></button>
-                        </div>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="/assets/profil/anak.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Selamat Datang di UPTD PSSA Harapan Bangsa Dinas Sosial Provinsi Lampung
-                                    </h5>
-                                    <p>Sasaran UPTD PSAA Harapan Bangsa dalam pelayanan, pembinaan dan rehabilitasi
-                                        pelayanan sosial asuhan anak meliputi anak yatim, piatu, yatim piatu, dan anak dari
-                                        keluarga yang tidak mampu usia SD, SLTP, dan SLTA.
+@section('page-style')
+    <style>
+        .carousel-item {
+            height: 100vh;
+            overflow: hidden;
+            position: relative;
+        }
 
-                                        Alamat: jl.Lettu Rohani No. 6 Desa Kedaton Kecamatan Kalianda
-                                        Kabupaten Lampung Selatan</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Gerbang.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Gerbang Depan</h5>
-                                    <p>Sebagai simbol keamanan dan ketertiban, memastikan lingkungan yang aman bagi siswa
-                                        dan pengunjung.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Kantor.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Kantor</h5>
-                                    <p>Sebagai tempat pusat administrasi dan layanan bagi siswa serta tenaga pengajar</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Asrama1.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Asrama 1</h5>
-                                    <p>Tempat tinggal yang nyaman dan bersih, menciptakan suasana kekeluargaan bagi siswa
-                                        dalam menjalani kehidupan sehari-hari.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Asrama2.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Asrama 2</h5>
-                                    <p>Tempat tinggal yang nyaman dan bersih, menciptakan suasana kekeluargaan bagi siswa
-                                        dalam menjalani kehidupan sehari-hari.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/aula.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Aula</h5>
-                                    <p>Ruang serbaguna yang digunakan untuk berbagai kegiatan seperti seminar, pertemuan,
-                                        dan acara keagamaan maupun kebudayaan.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Lapangan.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Lapangan</h5>
-                                    <p>Area terbuka untuk olahraga dan aktivitas fisik guna menunjang kesehatan dan
-                                        kebugaran siswa.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/RuangBelajar.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Ruang Belajar</h5>
-                                    <p>Didesain khusus untuk menciptakan lingkungan belajar yang kondusif, mendukung proses
-                                        pendidikan yang efektif.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Dapur.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Dapur</h5>
-                                    <p>Tempat penyediaan makanan dengan standar kebersihan, memastikan kebutuhan gizi siswa
-                                        terpenuhi.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Garasi_Parkiran.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Garasi</h5>
-                                    <p>Area penyimpanan kendaraan operasional untuk mendukung mobilitas dan kelancaran
-                                        aktivitas harian.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/RumahDinas1.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Rumah Dinas</h5>
-                                    <p>Hunian bagi staf dan tenaga pengajar, memastikan kenyamanan bagi mereka yang bertugas
-                                        di lingkungan UPTD PSA Harapan Bangsa.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/assets/profil/Musholla.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Mushola</h5>
-                                    <p>Tempat ibadah yang nyaman dan bersih, mendukung pembinaan spiritual serta kegiatan
-                                        keagamaan siswa.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                            data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                            data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
+        .carousel-item img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            position: absolute;
+            top: 0;
+            left: 0;
+            animation: pan-image 25s linear infinite;
+            border-radius: 10px;
+        }
+
+        .carousel-caption {
+            background: rgba(0, 0, 0, 0.5);
+            padding: 20px;
+            border-radius: 10px;
+            bottom: 20%;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 80%;
+            max-width: 600px;
+        }
+
+        .carousel-caption h2 {
+            font-size: 30px;
+            font-weight: bold;
+        }
+
+        .carousel-caption p {
+            font-size: 20px;
+        }
+
+
+
+        @media screen and (max-width: 768px) {
+            .carousel-item {
+                height: 150vh;
+            }
+
+            .carousel-item img {
+                width: auto !important;
+            }
+
+            .carousel-caption {
+                display: block !important;
+                bottom: 10%;
+                width: 90%;
+            }
+
+            .carousel-caption h2 {
+                font-size: 25px;
+            }
+
+            .carousel-caption p {
+                font-size: 25px;
+            }
+
+            @keyframes pan-image {
+                0% {
+                    transform: translateX(-30%);
+                }
+
+                100% {
+                    transform: translateX(-100%);
+                }
+            }
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="6000">
+        <div class="carousel-indicators">
+            @for ($i = 0; $i < 12; $i++)
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{ $i }}"
+                    class="{{ $i === 0 ? 'active' : '' }}" aria-label="Slide {{ $i + 1 }}"></button>
+            @endfor
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/assets/profil/anak.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Selamat Datang di UPTD PSSA Harapan Bangsa Dinas Sosial Provinsi Lampung
+                    </h2>
+                    <p>Sasaran UPTD PSAA Harapan Bangsa dalam pelayanan, pembinaan dan rehabilitasi
+                        pelayanan sosial asuhan anak meliputi anak yatim, piatu, yatim piatu, dan anak dari
+                        keluarga yang tidak mampu usia SD, SLTP, dan SLTA.
+
+                        Alamat: jl.Lettu Rohani No. 6 Desa Kedaton Kecamatan Kalianda
+                        Kabupaten Lampung Selatan</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Gerbang.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Gerbang Depan</h2>
+                    <p>Sebagai simbol keamanan dan ketertiban, memastikan lingkungan yang aman bagi siswa
+                        dan pengunjung.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Kantor.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Kantor</h2>
+                    <p>Sebagai tempat pusat administrasi dan layanan bagi siswa serta tenaga pengajar</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Asrama1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Asrama 1</h2>
+                    <p>Tempat tinggal yang nyaman dan bersih, menciptakan suasana kekeluargaan bagi siswa
+                        dalam menjalani kehidupan sehari-hari.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Asrama2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Asrama 2</h2>
+                    <p>Tempat tinggal yang nyaman dan bersih, menciptakan suasana kekeluargaan bagi siswa
+                        dalam menjalani kehidupan sehari-hari.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/aula.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Aula</h2>
+                    <p>Ruang serbaguna yang digunakan untuk berbagai kegiatan seperti seminar, pertemuan,
+                        dan acara keagamaan maupun kebudayaan.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Lapangan.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Lapangan</h2>
+                    <p>Area terbuka untuk olahraga dan aktivitas fisik guna menunjang kesehatan dan
+                        kebugaran siswa.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/RuangBelajar.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Ruang Belajar</h2>
+                    <p>Didesain khusus untuk menciptakan lingkungan belajar yang kondusif, mendukung proses
+                        pendidikan yang efektif.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Dapur.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Dapur</h2>
+                    <p>Tempat penyediaan makanan dengan standar kebersihan, memastikan kebutuhan gizi siswa
+                        terpenuhi.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Garasi_Parkiran.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Garasi</h2>
+                    <p>Area penyimpanan kendaraan operasional untuk mendukung mobilitas dan kelancaran
+                        aktivitas harian.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/RumahDinas1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Rumah Dinas</h2>
+                    <p>Hunian bagi staf dan tenaga pengajar, memastikan kenyamanan bagi mereka yang bertugas
+                        di lingkungan UPTD PSA Harapan Bangsa.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/profil/Musholla.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h2>Mushola</h2>
+                    <p>Tempat ibadah yang nyaman dan bersih, mendukung pembinaan spiritual serta kegiatan
+                        keagamaan siswa.</p>
                 </div>
             </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 @endsection
