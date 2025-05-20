d2ceeddb6ac00441fe1c4b60298c035536e915d0
ryan-ern <ryanernanda100902@gmail.com>
2025-05-20 22:25:06 +0700

    add (date validation on update)

--- app/Http/Controllers/pages/DataKegiatan.php
+++ app/Http/Controllers/pages/DataKegiatan.php
@@ -57,12 +57,13 @@ class DataKegiatan extends Controller
     $validator = Validator::make($request->all(), [
       'kegiatan' => ['required', 'string', 'max:255'],
       'waktu_mulai' => ['required', 'date'],
-      'waktu_selesai' => ['required', 'date'],
+      'waktu_selesai' => ['required', 'date', 'after_or_equal:waktu_mulai'],
       'keterangan' => ['required', 'string'],
     ], [
       'kegiatan.required' => 'Kegiatan harus diisi.',
       'waktu_mulai.required' => 'Waktu mulai harus diisi.',
       'waktu_selesai.required' => 'Waktu selesai harus diisi.',
+      'waktu_selesai.after_or_equal' => 'Waktu selesai tidak boleh lebih awal dari waktu mulai.',
       'keterangan.required' => 'Keterangan harus diisi.',
     ]);
 
